7db76925d521f88f34315acb007f2546366883ed
Poviuxs <129331922+Poviuxs@users.noreply.github.com>
2024-09-29 22:17:54 +0300

    Project5_Comparisons_Type_Coercion Assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,26 @@
+//variable with a Boolean dat type 
+var y=true;
+//printng the data type of the variable y
+document.write(typeof y);
+document.write("<br>")
+//an expression combining a string and number
+document.write("15"+10);
+document.write("<br>")
+//utilizing the == operator 
+document.write(10==10);
+document.write("<br>")
+//utilizing the === operator 
+x=5;
+y=5;
+document.write(x===y);
+document.write("<br>")
+//utilizing the >and && operators
+document.write(15>10 && 12>7);
+document.write("<br>")
+//utilizing the <and || operators
+document.write(14<7 || 13<7);
+document.write("<br>")
+//utilizing the ! operator
+function not_function(){
+document.getElementById("Not").innerHTML=!(1>2);
+}
