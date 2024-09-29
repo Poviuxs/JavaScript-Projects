a2772375708b01462cbfa1ec2f8f79e9e0f1b070
Poviuxs <129331922+Poviuxs@users.noreply.github.com>
2024-09-29 03:15:45 +0300

    Basic JavaScript Projects Assignment module 1 JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+
+// Assigning the 1st string to the Sent1 variable
+var Sent1 = "This is the beginning of the string";
+
+// Assigning the 2nd string to the Sent2 variable
+var Sent2 = " and this is the end of the string.";
+
+// Concatenating Sent1 and Sent2 into one full sentence
+var fullSentence = Sent1 + Sent2;
+
+/*
+Using window.alert() to display 
+*/
+window.alert(fullSentence);
+
+/*
+ concatenated sentence directly
+in the HTML body.
+*/
+document.write(30*20);
